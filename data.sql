af706f6744f737a2d185486d32a445edd088090d
paul-tes <pauasca8462@gmail.com>
2023-07-12 16:45:32 +0300

    Modifay animal table based on the onwers id and species

--- data.sql
+++ data.sql
@@ -33,3 +33,23 @@ INSERT INTO species (name)
 VALUES 
   ('Pokemon'),
   ('Digimon');
+
+-- Modify your inserted animals so it includes the species_id value
+UPDATE animals
+SET species_id = 
+	CASE 
+		WHEN NAME LIKE '%mon'
+			THEN (SELECT id FROM species WHERE name = 'Digimon')
+			ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+		END;
+  
+  -- Modify your inserted animals to include owner information (owner_id)
+UPDATE animals
+SET owner_id = 
+	CASE 
+		WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+		WHEN name IN ('Gabumon', 'Pikachu') THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+      WHEN name IN ('Devimon', 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+      WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+      WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+END;
