a03243fb75a932122969f67e41eaf9fb1e32405f
paul-tes <pauasca8462@gmail.com>
2023-07-12 15:55:52 +0300

    Add owner and species table

--- data.sql
+++ data.sql
@@ -8,11 +8,28 @@ VALUES
   ('Devimon', '2017-05-12', 5, true, 11);
 
   -- second round sample data.
-  INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES 
-('Charmander', '2020-02-08', 0, FALSE, -11),
-('Plantmon', '2021-11-15', 2, TRUE, -5.7),
-('Squirtle', '1993-04-02', 3, FALSE, -12.13),
-('Angemon', '2005-06-12', 1, TRUE, -45),
-('Boarmon', '2005-06-07', 7, TRUE, 20.4),
-('Blossom', '1998-10-13', 3, TRUE, 17),
-('Ditto', '2022-05-14', 4, TRUE, 22);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES 
+  ('Charmander', '2020-02-08', 0, FALSE, -11),
+  ('Plantmon', '2021-11-15', 2, TRUE, -5.7),
+  ('Squirtle', '1993-04-02', 3, FALSE, -12.13),
+  ('Angemon', '2005-06-12', 1, TRUE, -45),
+  ('Boarmon', '2005-06-07', 7, TRUE, 20.4),
+  ('Blossom', '1998-10-13', 3, TRUE, 17),
+  ('Ditto', '2022-05-14', 4, TRUE, 22);
+
+-- owners sample data
+INSERT INTO owners (full_name, age)
+VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+-- species sample data
+INSERT INTO species (name)
+VALUES 
+  ('Pokemon'),
+  ('Digimon');
