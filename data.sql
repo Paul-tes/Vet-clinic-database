0e28200d81ddb82daef758f8082bef2bbc8eb109
paul-tes <pauasca8462@gmail.com>
2023-07-12 15:58:34 +0300

    Add Sample data to owners and species

--- data.sql
+++ data.sql
@@ -19,3 +19,17 @@ VALUES
   ('Ditto', '2022-05-14', 4, TRUE, 22);
 
 -- owners sample data
+INSERT INTO owners (full_name, age)
+VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+-- species sample data
+INSERT INTO species (name)
+VALUES 
+  ('Pokemon'),
+  ('Digimon');
