012ca74ab5484405373b8f974714c17fed81bc10
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-17 18:59:00 +0300

    Create Index

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 /* Database schema to keep the structure of entire database. */
-
 CREATE DATABASE vet_clinic;
 
 -- Create [ animal ] table
@@ -9,41 +8,51 @@ CREATE TABLE animals (
   date_of_birth DATE,
   escape_attempts INT,
   neutered BOOLEAN,
-  weight_kg DECIMAL(5,2)
+  weight_kg DECIMAL(5, 2)
 );
 
 -- Add column species in animals table
-ALTER TABLE animalS
-ADD COLUMN species VARCHAR(255);
+ALTER TABLE
+  animalS
+ADD
+  COLUMN species VARCHAR(255);
 
 -- Add owners table
 CREATE TABLE owners (
-    id SERIAL PRIMARY KEY, -- [SERIAL] is for autoincreament.
-    full_name VARCHAR(255),
-    age INT
+  id SERIAL PRIMARY KEY,
+  -- [SERIAL] is for autoincreament.
+  full_name VARCHAR(255),
+  age INT
 );
 
 -- add species table
-CREATE TABLE species (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255)
-);
+CREATE TABLE species (id SERIAL PRIMARY KEY, name VARCHAR(255));
 
 -- drop id column
-ALTER TABLE animals DROP COLUMN id;
+ALTER TABLE
+  animals DROP COLUMN id;
 
 -- add auto increment primary id to animals table
-ALTER TABLE animals ADD COLUMN id SERIAL PRIMARY KEY;
+ALTER TABLE
+  animals
+ADD
+  COLUMN id SERIAL PRIMARY KEY;
 
 -- drop species table
-ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE
+  animals DROP COLUMN species;
 
 -- Add column species_id which is a foreign key referencing species table
-ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
+ALTER TABLE
+  animals
+ADD
+  COLUMN species_id INT REFERENCES species(id);
 
 -- Add column owner_id which is a foreign key referencing the owners table
-ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
-
+ALTER TABLE
+  animals
+ADD
+  COLUMN owner_id INT REFERENCES owners(id);
 
 -- Add Vets table
 CREATE TABLE vets (
@@ -58,7 +67,8 @@ CREATE TABLE specializations (
   vet_id INT,
   species_id INT,
   PRIMARY KEY (vet_id, species_id),
-  FOREIGN KEY (vet_id) REFERENCES vets(id) ON DELETE CASCADE, -- DEETE CASCADE is ON for to insure referential integrity on delelation of row.
+  FOREIGN KEY (vet_id) REFERENCES vets(id) ON DELETE CASCADE,
+  -- DEETE CASCADE is ON for to insure referential integrity on delelation of row.
   FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE CASCADE
 );
 
@@ -72,3 +82,8 @@ CREATE TABLE visits (
   FOREIGN KEY (vet_id) REFERENCES vets(id) ON DELETE CASCADE
 );
 
+CREATE INDEX visits_animals_id ON visits(animal_id);
+
+CREATE INDEX index_vet_id ON visits(vet_id);
+
+CREATE INDEX owners_email ON owners(email);
