30ea9021747c46802cb1c8ce9d65bb326ece06e5
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-18 17:41:09 +0300

    Create Table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -18,3 +18,23 @@ CREATE TABLE IF NOT EXISTS public.medical_histories (
     patient_id INT REFERENCES public.patients (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     status VARCHAR(255)
 );
+
+
+CREATE TABLE IF NOT EXISTS public.invoices
+(
+    id INT PRIMARY KEY,
+    total_amount DECIMAL(5, 2) NOT NULL,
+    generated_at DATE,
+    payed_at DATE,
+    medical_history_id INT REFERENCES public.medical_histories (id) ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+CREATE TABLE IF NOT EXISTS public.invoice_items
+(
+    id INT PRIMARY KEY,
+    unit_price DECIMAL(5, 2),
+    quantity INT,
+    total_price DECIMAL(5, 2),
+    invoice_id INT  REFERENCES public.invoices (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
+    treatment_id INT REFERENCES public.treatments (id) MATCH SIMPLE ON UPDATE NO ACTION
+);
